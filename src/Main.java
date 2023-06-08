de303f057085b7c90d3295d418c17c852cecb5b0
BozhidarKlepachev <135727167+BozhidarKlepachev@users.noreply.github.com>
2023-06-08 15:43:16 +0300

    Update Main from remote repository

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class MyClass {
     public static void main(String[] args) {
-        System.out.println("Hello World, Bozhidar");
+        System.out.println("Hello World, Bozhidar from remote repository");
     }
 }
